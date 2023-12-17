7e51824a9806ee0fb8575667c84845a20b4db294
Oleksii Fursov <38365227+OleksiiFursov@users.noreply.github.com>
2023-12-17 17:06:28 +0100

    db

--- server/db.mjs
+++ server/db.mjs
@@ -2,140 +2,228 @@ import mysql from 'mysql2/promise.js'
 import config from './config.mjs'
 
 const db = mysql.createConnection({
-  host: config.db.host,
-  user: config.db.user,
-  password: config.db.password,
-  database: config.db.name,
+    host: config.db.host,
+    user: config.db.user,
+    password: config.db.password,
+    database: config.db.name,
 })
 
-const operators = ['LIKE', 'NOT LIKE', '=', '!=', '<>', '>', '<', '>=', '<=', 'REGEXP', 'NOT REGEXP', 'IN', 'NOT IN', 'BETWEEN', 'NOT BETWEEN', 'IS NULL', 'IS NOT NULL', 'IS', 'MATCH']
+
+const operatorAlias = {
+    NIN: 'NOT IN',
+    NLIKE: 'NOT LIKE',
+    NNULL: 'NOT NULL',
+}
 
 class QueryBuilder {
-  constructor () {
-    this.column = []
-    this.fromTable = null
-    this._where = []
-    this.type = ''
-    this.table = ''
-  }
-
-  select (...columns) {
-    this.column = columns
-    this.type = 'select'
-    return this
-  }
-
-  from (table) {
-    this.table = table
-    return this
-  }
-
-  whereOr (conditions) {
-    return this._where(conditions, 'OR')
-  }
-
-  where (conditions, operatorLeft = 'AND') {
-    if (typeof conditions === 'number') {
-      conditions = { id: conditions }
+    constructor() {
+        this.column = []
+        this._where = []
+        this.type = null
+        this.table = null
+        this._limit = null;
+        this._offset = null;
+        this._order = [];
     }
 
-    this._where.push([conditions, operatorLeft])
-
-    return this
-  }
-
-  generateWhere () {
-    let res = ''
-    for (const [conditions, operatorJoin] of this._where) {
-      res += (res ? ' ' + operatorJoin : '') + this.generateWhereItem(conditions)
+    #err(msg) {
+        throw new Error(msg + JSON.stringify(this))
     }
-    return res
-  }
 
-  #addQuoteIfNeeded (value) {
-    return typeof value === 'string' ? '\'' + value + '\'' : value
-  }
 
-  generateWhereItem (conditions, operatorJoin = 'AND') {
-    let res = ''
-    for (const key in conditions) {
-      if (key === 'and' || key === 'or') {
-        res += '(' + conditions[key].map(v => this.generateWhereItem(v) + ' '+ key + ' ') + ')'
-      } else if (operators.includes(key)) {
-        console.log(11);
-      } else {
-        let value = conditions[key]
-        let operator = ' = '
-        if (typeof value === 'object') {
-          if (Array.isArray(value)) {
-            operator = ' IN '
-            value = '(' + value.map(cur => this.#addQuoteIfNeeded(cur)).join(', ') + ')'
-          }
+    select(...columns) {
+        this.column = columns
+        this.type = 'select'
+        return this
+    }
+
+    from(table) {
+        this.table = table
+        return this
+    }
+
+    limit(value, offset) {
+        this._limit = value;
+        if (offset) {
+            this._offset = offset;
         }
-        res += (res.length ? ' ' + operatorJoin : '') + ' ' + key + operator + value
-      }
+        return this;
     }
 
-    return res
-  }
-
-  run () {
-    if (!this.type) {
-      throw new Error('Query is not type. ' + JSON.stringify(this))
-    }
-    if (!this.table) {
-      throw new Error('Query is not from. ' + JSON.stringify(this))
+    order(field, direction = 'DESC') {
+        this._order.push(field, direction);
+        return this;
     }
 
-    if (this.type === 'select') {
-
-      return (
-        'SELECT ' + (this.column.length ? this.column.join(', ') : '*') +
-        ' FROM ' + this.table +
-        ' WHERE' + this.generateWhere()
-
-      )
+    whereOr(conditions) {
+        return this._where(conditions, 'OR')
     }
 
-    const selectClause = this.selectColumns.length > 0 ? `SELECT ${this.selectColumns.join(', ')}` : 'SELECT *'
-    const fromClause = this.fromTable ? `FROM \`${this.fromTable}\`` : ''
-    const whereClause = this.whereConditions.length > 0
-      ? `WHERE ${this.whereConditions.map(([column, value, operator]) =>
-        `\`${column}\` ${operator} ?`).join(' AND ')}`
-      : ''
+    where(conditions, operatorLeft = 'AND') {
+        if (typeof conditions === 'number') {
+            conditions = {id: conditions}
+        }
 
-    return `${selectClause} ${fromClause} ${whereClause}`
-  }
+        this._where.push([conditions, operatorLeft])
 
-  execute (connection, callback) {
-    const queryString = this.build()
-    const values = this.whereConditions.map(condition => condition[1])
+        return this
+    }
 
-    connection.query(queryString, values, (err, results) => {
-      callback(err, results)
-    })
-  }
+    generateWhere() {
+        let res = ''
+        for (const [conditions, operatorJoin] of this._where) {
+            res += (res ? ' ' + operatorJoin : '') + this.generateWhereItem(conditions)
+        }
+        return res
+    }
+
+    #addQuoteIfNeeded(value) {
+        return typeof value === 'string' ? '\'' + value + '\'' : value
+    }
+
+    generateWhereItem(conditions, operatorJoin = 'AND') {
+        let res = ''
+        for (const key in conditions) {
+            if (key === 'and' || key === 'or') {
+                res += ' ' + operatorJoin + ' ' + conditions[key].map(v => '(' + this.generateWhereItem(v) + ')').join(' ' + key.toUpperCase()+' ');
+                continue;
+            }
+            let value = conditions[key]
+            let operator = '='
+            if (typeof value === 'object') {
+
+                if (value === null) {
+                    operator = 'IS NULL'
+                    value = '';
+                } else {
+                    const keys = Object.keys(value);
+
+                    operator = keys[0] !== '0' ? keys[0].toUpperCase() : null;
+                    if (operatorAlias[operator])
+                        operator = operatorAlias[operator]
+
+                    if (Array.isArray(value) || operator === 'IN' || operator === 'NOT IN') {
+                        operator = operator || 'IN';
+                        value = '(' + value.map(cur => this.#addQuoteIfNeeded(cur)).join(', ') + ')'
+                    } else if (operator === 'LIKE' || operator=== 'NOT LIKE') {
+                        value = '"%' + value[keys[0]] + '%"';
+                    } else if (value instanceof RegExp) {
+                        value = value.toString().slice(-1, 1);
+                    } else if (['!=', '<>', '>', '<', '>=', '<='].includes(keys[0])) {
+                        res += '('+ keys.map(_key=> key + _key + value[_key]).join( ' ' + operatorJoin + ' ') + ')';
+                        continue;
+                    }
+                }
+
+
+            } else {
+                value = this.#addQuoteIfNeeded(value);
+            }
+            res += (res.length ? ' ' + operatorJoin + ' ' : '') + key + ' ' + operator + ' ' + value
+
+        }
+
+        return res
+    }
+
+
+    #runLimit() {
+        return (this._limit || this._offset ? ' LIMIT ' + (this._offset || 0) + ',' + (this._limit || Number.MAX_SAFE_INTEGER) : '');
+    }
+
+    #runOrder() {
+        if (!this._order.length) {
+            return '';
+        }
+        let res = 'ORDER BY';
+        for (const order of this._order) {
+            res += " " + order.join(' ');
+        }
+        return res;
+
+    }
+
+    run() {
+        if (!this.type) {
+            this.#err('Query is not type');
+        }
+        if (!this.table) {
+            this.#err('Query is not table');
+        }
+
+        if (this.type === 'select') {
+
+            return (
+                'SELECT ' + (this.column.length ? this.column.join(', ') : '*') +
+                ' FROM ' + this.table +
+                ' WHERE ' + this.generateWhere() +
+                this.#runOrder() +
+                this.#runLimit()
+
+
+            )
+        }
+    }
+
+    execute(connection, callback) {
+        const queryString = this.build()
+        const values = this.whereConditions.map(condition => condition[1])
+
+        connection.query(queryString, values, (err, results) => {
+            callback(err, results)
+        })
+    }
 }
 
 console.log(new QueryBuilder().select('name', 'age').from('users').where({
-  age: 12,
-  colors: ['red', 'green', 'blue'],
-  or: [
-    {
-      age: {
-        '<': 12,
-        '>': 18,
-      },
-      and: [
+    age: 12,
+    colors: ['red', 'green', 'blue'],
+    date_created: null,
+    or: [
         {
-          name: { LIKE: 'test' },
-          search: 'hh',
+            age: {
+                '<': 12,
+                '>': 18,
+            },
+            or: [
+                {
+                    name: {LIKE: 'test'},
+                },
+                {
+                    name: {LIKE: 'test2'},
+                },
+            ],
         },
-      ],
-    },
-  ],
+        {
+            gg: {'NLIKE': 'testgg'}
+        }
+    ],
 }).run())
+
+
+const build = () => new QueryBuilder();
+
+const createSchema = params => {
+    const q = this.build().from(params.table)
+    return {
+        find(where, columns = "*", run = 1) {
+            return q.select(columns).where(where).run(run)
+        },
+        findOne(where, columns = "*", run = 1) {
+            const res = q.select(columns).where(where).limit(1).run(run);
+            if (res && res[0]) {
+                return res[0];
+            }
+        },
+        select(columns = '*') {
+            return q.select(columns)
+        }
+    }
+}
+
+
+export {db, build, createSchema};
+
 export default {
-  db,
-  build: () => new QueryBuilder(),
+    db, build, createSchema
 }
---
+++ server/schemas/users.js
@@ -0,0 +1,9 @@
+import {createSchema} from '../db.mjs';
+export default createSchema({
+    table: 'users',
+    columns: {
+        id: []
+
+
+    }
+})
